a7b5d051bf4524810ce9d2245af1fd644b51ecbc
johnwmec <80866948+johnwmec@users.noreply.github.com>
2025-11-11 14:41:14 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-
-const SW_VERSION='v2.3.4';
-self.addEventListener('install', (e)=>{ self.skipWaiting(); });
-self.addEventListener('activate', (e)=>{ e.waitUntil((async()=>{ await self.clients.claim(); })()); });
+const SW_VERSION='v2.4.2';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>self.clients.claim());
